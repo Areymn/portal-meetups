48fb1fa0377b5de30e4a86991df3fe0ef9d72f1c
Alexiel R. <alexielreyesmartin@gmail.com>
2025-01-06 19:25:07 +0100

    New endpoints para validar, modificar y confirmar fecha de un evento

--- Backend/src/routes/meetups.js
+++ Backend/src/routes/meetups.js
@@ -1,5 +1,7 @@
 'user strict';
 
+const { isAdmin } = require('../db/events.js');
+
 console.log('Rutas de meetups cargadas correctamente');
 
 // Importar dependencias
@@ -9,12 +11,15 @@ const { detailEvent } = import('../controllers/meetups/detailEvent');
 const { getEvents } = import('../controllers/meetups/sortEvent');
 const { signupEvent } = import('../controllers/meetups/signupEvent');
 const { editEvent } = import('../controllers/meetups/editEvent');
+const { validateEvent } = import('../controllers/meetups/validateEvent');
+const { authenticateAdmin } = import('../middlewares/authAdmin.js');
 const { authenticateUser } = import('../middlewares/authMiddleware.js');
 const router = express.Router(); // Crear una instancia del router de Express
 
 // ------------------------- RUTAS DE MEETUPS -------------------------
 
 // Rutas de meetups
+router.post('/admin/validate', authenticateAdmin, validateEvent);
 
 router.post('/create', authenticateUser, createEvent);
 
@@ -22,7 +27,7 @@ router.post('/signup', authenticateUser, signupEvent);
 
 router.post('/detail', detailEvent);
 
-router.post('/edit', editEvent)
+router.post('/edit', editEvent);
 
 router.post('/', getEvents);
 
