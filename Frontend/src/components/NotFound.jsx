328c4637821ba38b5a91acfb648726581eacd639
Steven Francisco Jaramillo <stifrank97@gmail.com>
2025-01-27 18:11:09 +0100

    Implementación de pagina not found

---
+++ Frontend/src/components/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404 - Página no encontrada</h1>
+      <p>La página que estás buscando no existe.</p>
+      <Link to="/" className="back-home">
+        Volver al inicio
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
